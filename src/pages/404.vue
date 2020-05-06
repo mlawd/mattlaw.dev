fa154e8fab4c29c882ac91b7127acf7cb51f1385
Matthew Law <me@mattlaw.dev>
2020-05-06 20:23:22 +0100

    404

---
+++ src/pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    <v-container fill-height>
+      <v-row class="ma-0" align="center" justify="center">
+        <v-col cols="12" class="text-center">
+          <figure>
+            <g-image src="~/assets/img/404.png" alt="404" />
+            <figcaption>
+              <a href="https://www.freepik.com/free-photos-vectors/design">
+                Design vector created by freepik - www.freepik.com
+              </a>
+            </figcaption>
+          </figure>
+        </v-col>
+      </v-row>
+    </v-container>
+  </Layout>
+</template>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,21 +7,8 @@
         </v-col>
         <v-col cols="12" md="6">
           <p>
-            The approach will not be easy. You are required to maneuver straight
-            down this trench and skim the surface to this point. The target area
-            is only two meters wide. It’s a small thermal exhaust port, right
-            below the main port. The shaft leads directly to the reactor system.
-            A precise hit will start a chain reaction which should destroy the
-            station. Only a precise hit will set up a chain reaction. The shaft
-            is ray-shielded, so you’ll have to use proton torpedoes. That’s
-            impossible, even for a computer. It’s not impossible. I used to
-            bull’s-eye womp rats in my T-sixteen back home. They’re not much
-            bigger than two meters. Man your ships! And may the Force be with
-            you!
+            &hellip;and this is coming soon!
           </p>
-        </v-col>
-        <v-col cols="12" md="4">
-          <h2 class="mt-0">Projects</h2>
         </v-col>
       </v-row>
     </v-container>
