a6f50f984e43a226226d7581101534034188f128
Matt Law <me@mattlaw.dev>
2023-09-07 19:59:30 +0100

    start-afresh

--- src/crossfade.js
+++
@@ -1,8 +0,0 @@
-import { crossfade, fade } from 'svelte/transition';
-
-const [send, receive] = crossfade({
-  duration: (d) => Math.sqrt(d * 300),
-  fallback: fade,
-});
-
-export { send, receive };
--- src/sanity-client.ts
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client';
-
-const client = sanityClient({
-  projectId: 'uflbfg8d',
-  dataset: 'production',
-  useCdn: true,
-});
-
-export default client;
