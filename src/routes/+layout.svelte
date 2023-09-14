b137c206df108026bc4791de457eec1e7d8f2102
Matt Law <me@mattlaw.dev>
2023-09-14 21:35:43 +0100

    add metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,28 @@
 	import { page } from '$app/stores';
 
 	$: isHome = $page.route.id === '/';
+
+	$: title = `mattlaw.dev | ${$page.data?.post?.title ?? 'Home'}`;
+	$: description =
+		$page.data?.post?.subtitle ??
+		'Just some rambling thoughts of a Lead Engineer';
+
+	$: type = $page.data?.post ? 'article' : '';
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:title" content={title} />
+	<meta property="og:type" content={type} />
+	<meta property="og:description" content={description} />
+	{#if type === 'article'}
+		{#each $page.data?.post?.tags as tag}
+			<meta property="article:tag" content={tag} />
+		{/each}
+	{/if}
+</svelte:head>
 
 <main class="p-8 w-full" class:light={!isHome}>
 	<div class="content">
